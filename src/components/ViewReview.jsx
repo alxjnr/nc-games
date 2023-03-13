0f9a53ad58676795d7a656817818562e2cac36da
alxjnr <alexrobertjoiner@gmail.com>
2023-03-13 14:22:43 +0000

    add the option to view/hide comments

--- src/components/ViewReview.jsx
+++ src/components/ViewReview.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
-import { getReviewById } from "../api";
+import { getReviewById, getCommentsOnReview } from "../api";
 import { useParams } from "react-router-dom";
 
 const ViewReview = () => {
   const [review, setReview] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [createdDate, setCreatedDate] = useState("");
+  const [comments, setComments] = useState([]);
+  const [isLoadingComments, setIsLoadingComments] = useState(false);
+  const [isReadingComments, setIsReadingComments] = useState(false);
 
   const { review_id } = useParams();
 
@@ -18,6 +21,21 @@ const ViewReview = () => {
       setIsLoading(false);
     });
   }, [review_id]);
+
+  const renderComments = (review_id) => {
+    if (isReadingComments) {
+      setComments([]);
+      setIsReadingComments(false);
+    } else {
+      setIsLoadingComments(true);
+      getCommentsOnReview(review_id).then((data) => {
+        setComments(data);
+        setIsLoadingComments(false);
+        setIsReadingComments(true);
+      });
+    }
+  };
+
   return (
     <section>
       {isLoading ? (
@@ -35,7 +53,35 @@ const ViewReview = () => {
           <section className="review-body-section">
             <h3>{review.review_body}</h3>
           </section>
-          <button>view comments ({review.comment_count})</button>
+          <button
+            onClick={() => {
+              renderComments(review_id);
+            }}
+          >
+            {!isReadingComments ? <p>view comments</p> : <p>hide comments</p>} (
+            {review.comment_count})
+          </button>
+          <section>
+            {isLoadingComments ? (
+              <h2>loading...</h2>
+            ) : (
+              <section>
+                {comments.map((comment) => {
+                  return (
+                    <section
+                      key={comment.created_at}
+                      className="comment-section"
+                    >
+                      <h5>{comment.author}</h5>
+                      <h4>{comment.body}</h4>
+                      <h5>votes: {comment.votes}</h5>
+                      <h5>{comment.created_at.slice(0, 10)}</h5>
+                    </section>
+                  );
+                })}
+              </section>
+            )}
+          </section>
         </section>
       )}
     </section>
